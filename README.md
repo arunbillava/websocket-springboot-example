139740c3c2dcf2701e180d30705339fb408f4e67
arunbillava <arunbillava3@gmail.com>
2020-05-25 10:13:36 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Example for WebSockets in a Spring Boot Application
+
+- `/topic/user` - Message Broker topic for pushing messages to the UI back.
+- `/websocket/hello` - welcome url
+ 
---
+++ src/main/java/com/techprimers/springbootwebsocketexample/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.techprimers.springbootwebsocketexample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/websocket")
+public class HomeController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Welcome to websocket";
+	}
+}
